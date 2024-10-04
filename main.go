99d4b144c35d558865f4f573a583c2bd252f184c
DESKTOP-4KO3KLJ\Admin <piv.pav@mail.ru>
2024-10-04 22:55:13 +0200

    добавление функции сложения и умножения

--- main.go
+++ main.go
@@ -5,5 +5,30 @@ import "fmt"
 
 
 func main() {
-    fmt.Println("Hello, Go!")
+    var a, b int
+    var oper string
+    fmt.Println("Введите первое число:")
+    fmt.Scan(&a)
+    fmt.Println("Выберете операцию:")
+    fmt.Scan(&oper)
+    fmt.Println("Введите второе число:")
+    fmt.Scan(&b)
+
+    if oper == "+" { 
+        sum(a, b)
+    }
+    
+    if oper == "*"{
+        mult(a, b)
+    }
+}
+
+// функция сложения
+func sum(a int, b int) int { 
+    return a + b
+}
+
+// функция сложения
+func mult(a int, b int) int { 
+    return a * b
 }
