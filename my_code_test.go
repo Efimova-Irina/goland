c43b2462b1fbba982aa97a3e33868b8cdf0c526b
DESKTOP-4KO3KLJ\Admin <piv.pav@mail.ru>
2024-10-06 19:26:29 +0200

    unit тест для деления в калькуляторе

--- my_code_test.go
+++ my_code_test.go
@@ -3,21 +3,23 @@ package main
 
 import "testing"
 
-func Test_Sum(t *testing.T) {
+func Test_Div(t *testing.T) {
  tests := []struct {
   name string
   a    int
   b    int
   want int
  }{
-  {"test 1", 0, 0, 0},
-  {"test 2", 0, 1, 1},
-  //    и тд
+  {"test 1", 1, 0, 0}, // примеры входных данных для проверки
+  {"test 2", 0, 1, 0},
+  {"test 3", -3, 2, -1},
+  {"test 4", -6, -3, 2},
+  {"test 5", 2, 3, 0},
  }
 
  for _, tt := range tests {
   t.Run(tt.name, func(t *testing.T) {
-   if got := sum(tt.a, tt.b); got != tt.want {
+   if got, _ := div(tt.a, tt.b); got != tt.want { // проверка операции, при делении выводится 2 аргумента, при др операциях 1 аргумент
     t.Errorf("Sum() = %v, want %v", got, tt.want)
    }
   })
